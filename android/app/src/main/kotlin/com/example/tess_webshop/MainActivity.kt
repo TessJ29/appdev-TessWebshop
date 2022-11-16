1a0f0ee3c985017539cec516ab8d9457e2e9971e
Tess Jansen <tess.jansen29@gmail.com>
2022-11-16 12:14:01 +0100

    app development eigen versie webshop

---
+++ android/app/src/main/kotlin/com/example/tess_webshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tess_webshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pages/products_overview_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:tess_webshop/models/product.dart';
+
+class ProductOverviewPage extends StatefulWidget {
+  const ProductOverviewPage({super.key});
+
+  @override
+  State<ProductOverviewPage> createState() => _ProductOverviewPageState();
+}
+
+class _ProductOverviewPageState extends State<ProductOverviewPage> {
+  List<Product> loadedProducts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 243, 158, 46),
+        title: Text('Tess Shop'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          gridDelegate: gridDelegate,
+          itemBuilder: itemBuilder,
+        ),
+      ),
+    );
+  }
+}
